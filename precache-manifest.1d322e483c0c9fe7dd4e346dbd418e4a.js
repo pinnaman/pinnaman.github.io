076a51c16f9f825b578b8cf8f8721096d8fc38bf
Ajay Pinnamaneni <apinnamaneni@dev-bbragg.local>
2020-03-10 11:56:22 -0400

    Updates

---
+++ precache-manifest.1d322e483c0c9fe7dd4e346dbd418e4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4899ba7d63eae6606ff893821d57eb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e794c7fcf91f737a7fb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "642650ace36d4bff6c0e",
+    "url": "/static/js/2.a990b339.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.a990b339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e794c7fcf91f737a7fb",
+    "url": "/static/js/main.91a76b6d.chunk.js"
+  },
+  {
+    "revision": "ae11ae770fe4ccff0627",
+    "url": "/static/js/runtime-main.e7b19972.js"
+  }
+]);
